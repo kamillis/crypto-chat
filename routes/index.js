15b3afbaed33ce2bacba577042d22f3808ec96b6
kamillis <kontakt@kamillis.pl>
2016-02-14 15:28:31 +0100

    Libs added

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Crypto Chat'
+    });
 });
 
 module.exports = router;
