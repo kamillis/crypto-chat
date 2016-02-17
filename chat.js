3e4079fb8dde46604e8fe3305f9d2ff6c98cb9fd
kamillis <kontakt@kamillis.pl>
2016-02-17 15:14:39 +0100

    Ciphering with proper keys

--- chat.js
+++ chat.js
@@ -1,9 +1,11 @@
 var socketIO = require('socket.io');
 var Crypto = require('./utils/crypto');
+var Users = require('./utils/users');
 
 module.exports = function(app, server) {
 
     var crypto = new Crypto();
+    var users = new Users();
 
     var io = socketIO(server, {
         path: '/chat'
@@ -12,9 +14,10 @@ module.exports = function(app, server) {
     io.on('connection', function(socket) {
 
         var chatReady = false;
-        var serverKeys, sessionKey;
+        var sessionKey;
 
         var initChat = function(user, room) {
+            users.addUser(room, user, socket, sessionKey);
 
             // Join socket to room
             socket.join(room);
@@ -23,8 +26,18 @@ module.exports = function(app, server) {
             socket.on('new message', function(message) {
                 var decMsg = crypto.decryptMessage(message, sessionKey);
                 var msg = Date.now() + '|' + decMsg;
-                var encMsg = crypto.encryptMessage(msg, sessionKey);
-                io.to(room).emit('new message', encMsg);
+                for (var k in users.storage[room]) {
+                    if (users.storage[room].hasOwnProperty(k)) {
+                        var user = users.storage[room][k];
+                        var encMsg = crypto.encryptMessage(msg, user.key);
+                        user.socket.emit('new message', encMsg);
+                    }
+                }
+            });
+
+            // Socket disconnect
+            socket.on('disconnect', function() {
+                users.deleteUser(room, user);
             });
 
             chatReady = true;
@@ -39,7 +52,7 @@ module.exports = function(app, server) {
 
         socket.on('client public key', function(key) {
             if (!chatReady) {
-                serverKeys = crypto.generateKeys();
+                var serverKeys = crypto.generateKeys();
                 sessionKey = crypto.getSessionKey(key, serverKeys.priKey);
                 socket.emit('server public key', serverKeys.pubKey);
             }
---
+++ utils/users.js
@@ -0,0 +1,19 @@
+function Users() {
+    this.storage = {};
+}
+
+Users.prototype.addUser = function(room, user, socket, key) {
+    if (!this.storage[room]) this.storage[room] = {};
+    if (!this.storage[room][user]) this.storage[room][user] = {};
+    this.storage[room][user]['socket'] = socket;
+    this.storage[room][user]['key'] = key;
+};
+
+Users.prototype.deleteUser = function(room, user) {
+    delete this.storage[room][user];
+    if (Object.keys(this.storage[room]).length == 0)
+        delete this.storage[room];
+};
+
+
+module.exports = Users;
